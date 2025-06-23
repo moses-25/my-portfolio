a14ce22b73996ca2b2df9ebc58a3015cf6f45201
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-06-23 22:53:01 +0300

    Add Message model to define structure for contact messages

---
+++ contact-portfolio-backend/app/models.py
@@ -0,0 +1,8 @@
+from . import db
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    subject = db.Column(db.String(150), nullable=False)
+    message = db.Column(db.Text, nullable=False)
