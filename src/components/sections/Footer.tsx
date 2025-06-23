4e8e967ee50b0a019008798ecb6f9f119496b994
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-06-23 17:59:23 +0300

    update Footer component: change builder name to OCHIENG MOSES OTIENO

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -92,7 +92,7 @@ const Footer: React.FC<FooterProps> = ({ darkMode }) => {
         
         <div className="border-t border-gray-700 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p>&copy; {new Date().getFullYear()} Moses Otieno. All rights reserved.</p>
-          <p className="mt-4 md:mt-0">Built by <span className="text-red-500">@</span>moses_kingstone</p>
+          <p className="mt-4 md:mt-0">Built by <span className="text-red-500">🔅</span>OCHIENG MOSES OTIENO</p>
         </div>
       </div>
     </footer>
